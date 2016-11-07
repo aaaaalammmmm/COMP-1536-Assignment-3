85eb7e5066639c661137f26591096fbd6be3f1a0
Robbie Perry <robbieperry22@gmail.com>
2016-11-06 18:26:15 -0800

    Completed basic structure of house

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function start() {
     canvasHeight = canvas.height;
     drawBackground();
     drawHouseFront();
+    drawHouseLeft();
+    drawRoof();
 }
 
 function drawBackground() {
@@ -42,19 +44,65 @@ function drawBackground() {
 }
 
 function drawHouseFront() {
-    var frontLeftX = 100;
-    var frontRightX = 275;
-    var topLeftY = 150;
-    var frontWallHeight = 100;
-    var topRightY = 120;
+    var frontLeftX = 140;
+    var frontRightX = 340;
+    var topLeftY = 250;
+    var frontWallHeight = 110;
+    var topRightY = 240;
     
     ctx.beginPath();
     ctx.moveTo(frontLeftX, topLeftY + frontWallHeight);
     ctx.lineTo(frontLeftX, topLeftY);
     ctx.lineTo(frontRightX, topRightY);
-    ctx.lineTo(frontRightX, topRightY + frontWallHeight);
+    ctx.lineTo(frontRightX, topRightY + frontWallHeight - 25);
     ctx.lineTo(frontLeftX, topLeftY + frontWallHeight);
-    ctx.fillStyle = "yellow";
+    ctx.fillStyle = "#EDED77";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawHouseLeft() {
+    var leftLeftX = 50;
+    var leftRightX = 140;
+    
+    var middleRightY = 250;
+    var middleLeftY = 240;
+    
+    var leftSummitX = 80;
+    var leftSummitY = 180;
+    
+    ctx.beginPath();
+    ctx.moveTo(leftRightX, middleRightY + 110);
+    ctx.lineTo(leftRightX, middleRightY);
+    ctx.lineTo(leftSummitX, leftSummitY);
+    ctx.lineTo(leftLeftX, middleLeftY);
+    ctx.lineTo(leftLeftX, middleRightY + 80);
+    ctx.moveTo(leftRightX, middleRightY + 110);
+    ctx.fillStyle = "#D1D169";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawRoof() {
+    var leftSummitX = 80;
+    var leftSummitY = 180;
+    
+    var rightSummitX = 260;
+    var rightSummitY = 180;
+    
+    var roofLeftX = 140;
+    var roofLeftY = 250;
+    
+    var roofRightX = 340;
+    var roofRightY = 240;
+    
+    ctx.beginPath();
+    ctx.moveTo(roofLeftX, roofLeftY);
+    ctx.lineTo(leftSummitX, leftSummitY);
+    ctx.lineTo(rightSummitX, rightSummitY);
+    ctx.lineTo(roofRightX, roofRightY);
+    ctx.lineTo(roofLeftX, roofLeftY);
+    ctx.fillStyle = "#E61029";
     ctx.fill();
     ctx.closePath();
 }
